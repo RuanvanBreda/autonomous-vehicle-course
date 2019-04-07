e61dbaa0c1c3867766f5523b6186d643643c1536
RuanvanBreda <vanbredaruanjacobus@gmail.com>
2019-04-07 18:00:33 +0200

    Convert Image to Grayscale

--- Section 5 CV:Finding Lane Lines.py
+++ Section 5 CV:Finding Lane Lines.py
@@ -1,6 +1,14 @@
 #pip install opencv-contrib-python
 import cv2
+import numpy as np
 
 image = cv2.imread('Image/test_image.jpg')
-cv2.imshow('result',image)
+lane_image = image  #Make a copy of original image
+
+
+#Edge Detection using changes in intensity [0-255]
+#Rapid change in pixel density
+gray_image = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY) #Grayscale Conversion
+cv2.imshow('result',gray_image)
 cv2.waitKey(0)  #Displays image untill further input is received
+
