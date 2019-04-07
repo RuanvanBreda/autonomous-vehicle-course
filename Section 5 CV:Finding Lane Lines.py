e7a09609bb289bd0cf0d84a78c87c88a78b0c8e3
RuanvanBreda <vanbredaruanjacobus@gmail.com>
2019-04-07 18:05:57 +0200

    Blurr Greyscale Image to Reduce Noise

--- Section 5 CV:Finding Lane Lines.py
+++ Section 5 CV:Finding Lane Lines.py
@@ -9,6 +9,9 @@ lane_image = image  #Make a copy of original image
 #Edge Detection using changes in intensity [0-255]
 #Rapid change in pixel density
 gray_image = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY) #Grayscale Conversion
-cv2.imshow('result',gray_image)
-cv2.waitKey(0)  #Displays image untill further input is received
 
+#Smooth Image using Gaussian Blurr to reduce noise - noise can create false edges - use a kernel
+blur_image = cv2.GaussianBlur(gray_image,(5,5),0)   #original image, size of kernel, deviation
+
+cv2.imshow('result',blur_image)
+cv2.waitKey(0)  #Displays image untill further input is received
